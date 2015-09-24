4dd5ebe8021a47910286fa8ffb2556b2a9cb1727
George Calderon <gcald@csu.fullerton.edu>
2015-09-23 20:29:19 -0700

    point.cpp
    Point class done

--- point.cpp
+++ point.cpp
@@ -5,26 +5,21 @@
 * ------------------------------------------------------------------------- */
 
   /**
-   * A short program to print "Hello World!" to standard output.
+   * Describes a point in the Euclidean plane.
    */
 
-//Describes a point in the Euclidean plane.
-
-//Note: This means that the x and y values need to be either float or double.
-
-//Must be able to be initialized with no arguments, defaulting to the point (0,0).
-
-//Note: This is the same as saying that it must have a default constructor. If a constructor has default values for all arguments, it fulfills this requirement and is considered a default constructor.
-
-//Must be able to be initialized with an x and y value.
-
-//Must not be possible to change the x or y value once it has been constructed.
-
-//Note: This means that the member variables containing the x and y values must be private (or protected), and you must implement getters (but not setters) for each.
+#include "point.h"
 
 class point{
 	private:
 		double x, y;
+	public:
+		point () { x = 0,y = 0;}
+		point (double a, double b) : x(a), y(b) {}
+		double getX(){
+			return x;
+		}
+		double getY(){
+			return y;
+		}
 };
-
-point::point() : x(0) {}

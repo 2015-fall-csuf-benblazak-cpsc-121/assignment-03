e220bc7ee452ea2fda38d96bd95fa00d8abc1ddb
Provect <jamespmabe@gmail.com>
2015-09-24 10:53:59 -0700

    Create point.cpp

---
+++ point.cpp
@@ -0,0 +1,21 @@
+/* ----------------------------------------------------------------------------
+ * Copyright &copy; 2015 James Mabe <jamespmabe@gmail.com>
+ * Released under the [MIT License] (http://opensource.org/licenses/MIT)
+ * References: Given by Mr. Blazak
+ * ------------------------------------------------------------------------- */
+
+/**
+ * unknown.
+ */
+
+#include <cmath>
+#include "point.h"
+using std::pow;
+using std::sqrt;
+
+
+Point::Point(float x, float y) : x(x), y(y) {}
+
+float Point::distance(const Point & a) const{
+    return sqrt( pow(a.x - this ->x, 2) + pow(a.y - this->y, 2));
+}

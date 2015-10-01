b7d606a94905fa2125a6f52ef1dec2193af67725
Ubiquiphile <nerd_wizard@csu.fullerton.edu>
2015-10-01 11:15:55 -0700

    Update shapes.h

--- shapes.h
+++ shapes.h
@@ -1,62 +1,36 @@
-/* ----------------------------------------------------------------------------
- * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
- * Released under the [MIT License] (http://opensource.org/licenses/MIT)
- * ------------------------------------------------------------------------- */
-
-/**
- * Classes representing shapes in a Euclidean plane.
- */
-
-#ifndef SHAPES_H
-#define SHAPES_H
-// ----------------------------------------------------------------------------
-
 #include "point.h"
 
-class Shape {
-    public:
-        virtual ~Shape() {}
-        virtual bool contains(const Point & p) const = 0;  // pure virtual
+class Shape{
+
+private:
+	
+public:
+
+	virtual bool constains(const Point & p) const = 0;
+	virtual ~Shape();
+
 };
 
-// ----------------------------------------------------------------------------
+class Rectangle : public Shape{
+private:
+	Point corner;
+	int length;
+	int width;
+public:
 
-class Rectangle : public Shape {
-    private:
-        Point center;
-        float dx;
-        float dy;
-
-    public:
-        Rectangle(Point center = Point(), float dx = 0, float dy = 0);
-
-        bool contains(const Point & p) const;
+	Rectangle(Point corner = Point(0, 0), int length = 0, int width = 0);
+	bool constains(const Point & p) const;
+	~Rectangle();
 };
 
-class Square : public Rectangle {
-    public:
-        Square(Point center = Point(), float side = 0);
+class Ellipse : public Shape{
+
 };
 
-// ----------------------------------------------------------------------------
+class Square : public Rectangle{
 
-class Ellipse : public Shape {
-    private:
-        Point f1;
-        Point f2;
-        float radius;
-
-    public:
-        Ellipse(Point f1 = Point(), Point f2 = Point(), float radius = 0);
-
-        bool contains(const Point & p) const;
 };
 
-class Circle : public Ellipse {
-    public:
-        Circle(Point center = Point(), float radius = 0);
+class Circle : public Ellipse{
+
 };
-
-// ----------------------------------------------------------------------------
-#endif  // SHAPES_H
-

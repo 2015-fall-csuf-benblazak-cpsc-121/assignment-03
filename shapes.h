2ff15ab54b2eec1df8a9d469ea8e143126c80dcf
Provect <jamespmabe@gmail.com>
2015-10-01 10:53:27 -0700

    Update shapes.h

--- shapes.h
+++ shapes.h
@@ -4,7 +4,7 @@
  * ------------------------------------------------------------------------- */
 
 /**
- * This header file contains the superclass of classes in shapes.cpp
+ * This header file contains the classes of the shapes
  */
 
 #ifndef SHAPES_H
@@ -19,5 +19,38 @@ public:
     
 };
 
-#endif // SHAPES_H
+class Rectangle : public Shape {
+private:
+    Point center;
+    float Rx;
+    float Ry;
+    
+public:
+    Rectangle(Point center = Point(), float Rx = 0, float Ry = 0);
+    
+    bool contains(const Point & p) const;
+};
 
+class Ellipse : public Shape{
+private:
+    Point f1;
+    Point f2;
+    float radius;
+    
+public:
+    Ellipse(Point f1 = Point(), Point f2 = Point(), float radius = 0);
+    
+    bool contains (const Point & p) const;
+};
+
+class Square : public Rectangle{
+public:
+    Square(Point center = Point(), float side = 0);
+};
+
+class Circle : public Ellipse{
+public:
+    Circle(Point center = Point(), float radius = 0);
+};
+
+#endif // SHAPES_H

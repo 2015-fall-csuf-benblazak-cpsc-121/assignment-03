20a075c2da8a30f1e1251a944b689af08ce99e95
Ubiquiphile <nerd_wizard@csu.fullerton.edu>
2015-10-01 11:15:15 -0700

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -1,22 +1,13 @@
-/* ----------------------------------------------------------------------------
- * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
- * Released under the [MIT License] (http://opensource.org/licenses/MIT)
- * ------------------------------------------------------------------------- */
-
-#include <cmath>
-using std::pow;
-using std::sqrt;
-
 #include "point.h"
 
-// ----------------------------------------------------------------------------
+Point::Point(const float x = 0, const float y = 0) {
 
-Point::Point(float x, float y) : x(x), y(y) {}
-
-float Point::getX() const { return x; }
-float Point::getY() const { return y; }
-
-float Point::distance(const Point & a) const {
-    return sqrt( pow( a.x - this->x, 2 ) + pow( a.y - this->y, 2 ) );
+}
+	
+float Point::getX() const {
+	return x;
 }
 
+float Point::getY() const {
+	return y;
+}

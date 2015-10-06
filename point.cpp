a3fbcbeb38e198bb26a5c88a0f15da204089c34c
hympert <hympert@gmail.com>
2015-10-05 22:59:01 -0700

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -1,4 +1,13 @@
-void Point::get_point(const float xpos, const float ypos){
-	x = xpos;
-	y = ypos;
+Point::Point(float xcoord, float ycoord) : X(xcoord), Y(ycoord) {} //get x and y
+
+float Point::getX() const {
+	return X;
+}
+
+float Point::getY() const {
+	return Y;
+}
+
+float Point::calc_distance(const Point &a) const{
+	return sqrt(pow(this->X - a.X,2) + pow(this->Y - a.Y,2));
 }

29dae27a3ee6352b1004e62658d3ce2dd2892d77
hympert <hympert@gmail.com>
2015-10-05 22:58:30 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 // assignment3.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+
 #include <iostream>
-using std::cin;
 using std::cout;
 using std::endl;
 
@@ -16,22 +15,46 @@ using std::endl;
 
 
 
-void draw(const int count, const Shape * const s[]){
-	Point p;
+void draw(const int count, const Shapes * const s[]){
+	const int ROWS = 25;  //y value
+	const int COLS = 80;  //x value
+	const float ROWSCALE = 1.9;
+	const int COLSCALE = 1;
 
-	const int WINDOW_WIDTH = 80;
-	const int WINDOW_HEIGHT = 25;
+	for (int y = 0; y <= ROWS; y++){
+		float ypos = y * ROWSCALE;
 
-	for (int rows = 0; rows < 25; rows++){
-		for (int cols = 0; cols < 80; cols++){
+		for (int x = 0; x <= COLS; x++){
+			float xpos = x * COLSCALE;
 
+			bool includePoint = false;
+
+			for (int c = 0; c < count; c++)
+			if (s[c]->contains(Point(xpos, ypos))){
+				includePoint = true;
+			}
+
+			if (includePoint){
+				cout << "*";
+			}
+			else{
+				cout << " ";
+			}
+				
 		}
+		cout << endl;
 	}
 }
 
-int _tmain(int argc, _TCHAR* argv[])
-{
 
+int main(){
+
+	Ellipse s(15,25,30);
+	Circle c(50,20);
+
+	Shapes * shapes[] = { &s, &c, };
+	
+	draw(sizeof(shapes) / sizeof(Shapes *), shapes);
 
 	return 0;
 }

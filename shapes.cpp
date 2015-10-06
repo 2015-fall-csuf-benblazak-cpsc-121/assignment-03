23c7695574c79050925bd7bb7cfaf357519910e6
hympert <hympert@gmail.com>
2015-10-05 23:00:35 -0700

    Update shapes.cpp

--- shapes.cpp
+++ shapes.cpp
@@ -1,1 +1,23 @@
+#ifndef
 
+class Shapes{
+public:
+	virtual bool contains(const Point &p) const = 0;
+	virtual ~Shapes(){}
+};
+
+class Ellipse : public Shapes{
+public:
+	Ellipse(Point F1 = 0, Point F2 = 0, float dis_focal_to_edge = 0);
+	bool contains(const Point &p) const;
+private:
+	Point F1;
+	Point F2;
+	float dis_focal_to_edge;
+};
+
+class Circle : public Ellipse{
+public:
+	Circle(Point center = 0, float radius = 0);
+ 
+};
